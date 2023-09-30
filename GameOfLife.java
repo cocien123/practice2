1fae31f89b6a4255c4d372892cc6f97f1c4e644b
cocien123 <126736558+cocien123@users.noreply.github.com>
2023-09-30 21:38:11 +0400

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,77 +1,128 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+
+class Cell {
+    private int x, y;
+
+    Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public List<Cell> getNeighbours() {
+        List<Cell> neighbours = new ArrayList<>();
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                if (dx == 0 && dy == 0) continue;
+                neighbours.add(new Cell(x + dx, y + dy));
+            }
+        }
+        return neighbours;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cell)) return false;
+        Cell cell = (Cell) o;
+        return x == cell.x && y == cell.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
+    }
+}
+
 public class GameOfLife {
-    private int width;
-    private int height;
-    private boolean[][] board;
+    private Set<Cell> liveCells = new HashSet<>();
 
-    GameOfLife(int width, int height) {
-        this.width = width;
-        this.height = height;
-        this.board = new boolean[height][width];
+    public GameOfLife(List<Cell> initialLiveCells) {
+        liveCells.addAll(initialLiveCells);
     }
 
-    public void setAlive(int x, int y) {
-        board[y][x] = true;
+    public Set<Cell> getLiveCells() {
+        return liveCells;
     }
 
-    public boolean isAlive(int x, int y) {
-        return board[y][x];
-    }
+    public void nextGeneration() {
+        Set<Cell> newLiveCells = new HashSet<>();
+        Set<Cell> potentialCells = new HashSet<>(liveCells);
 
-    public int countNeighbors(int x, int y) {
-        int count = 0;
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if (i == 0 && j == 0) continue;
-                if (x + j >= 0 && x + j < width && y + i >= 0 && y + i < height) {
-                    if (board[y + i][x + j]) count++;
+        for (Cell liveCell : liveCells) {
+            potentialCells.addAll(liveCell.getNeighbours());
+        }
+
+        for (Cell cell : potentialCells) {
+            int liveNeighbours = countLiveNeighbours(cell);
+            if (liveCells.contains(cell)) {
+                if (liveNeighbours == 2 || liveNeighbours == 3) {
+                    newLiveCells.add(cell);
                 }
+            } else {
+                if (liveNeighbours == 3) {
+                    newLiveCells.add(cell);
+                }
+            }
+        }
+
+        liveCells = newLiveCells;
+    }
+
+    private int countLiveNeighbours(Cell cell) {
+        int count = 0;
+        for (Cell neighbour : cell.getNeighbours()) {
+            if (liveCells.contains(neighbour)) {
+                count++;
             }
         }
         return count;
     }
 
-    public void nextGeneration() {
-        boolean[][] newBoard = new boolean[height][width];
+    private static void render(Set<Cell> cells) {
+        final int width = 70;
+        final int height = 20;
+        char[][] display = new char[height][width];
 
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int neighbors = countNeighbors(x, y);
-                if (board[y][x]) {
-                    newBoard[y][x] = neighbors == 2 || neighbors == 3;
-                } else {
-                    newBoard[y][x] = neighbors == 3;
-                }
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                display[i][j] = '.';
             }
         }
 
-        board = newBoard;
-    }
+        for (Cell cell : cells) {
+            if (cell.getX() >= 0 && cell.getX() < width && cell.getY() >= 0 && cell.getY() < height) {
+                display[cell.getY()][cell.getX()] = 'O';
+            }
+        }
 
-    public void display() {
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                System.out.print(board[y][x] ? "O" : ".");
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(display[i][j]);
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        GameOfLife game = new GameOfLife(10, 10);
+        List<Cell> glider = List.of(new Cell(1, 3), new Cell(2, 1), new Cell(2, 3), new Cell(3, 2), new Cell(3, 3));
+        GameOfLife game = new GameOfLife(glider);
 
-        // 设置初始状态，例如一个小型的"滑翔机"模式
-        game.setAlive(1, 0);
-        game.setAlive(2, 1);
-        game.setAlive(0, 2);
-        game.setAlive(1, 2);
-        game.setAlive(2, 2);
-
-        // 运行游戏
-        while (true) {
-            game.display();
+        for (int i = 0; i < 30; i++) {
+            System.out.println("Generation: " + (i + 1));
+            render(game.getLiveCells());
             game.nextGeneration();
-            Thread.sleep(1000); // 延迟 1 秒
-            System.out.println("\n\n\n");
+            Thread.sleep(1000);
         }
     }
 }
